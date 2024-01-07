b203b5241ad92784591a191539b88d50e7d482b5
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-01-07 23:22:45 +0530

    Add files via upload

---
+++ logical_op.py
@@ -0,0 +1,47 @@
+# Logical ooperators on relational or conditional statmets
+# Returns true or False based on Truth value
+
+# 1 and
+# 2 or 
+# 3 not
+# 1 use making decisions
+# 2 for controlling the flow of code 
+
+
+# print(py_long=="y")
+# print(marks>=23)
+print(True and True)
+print(False and True)
+print(True and False)
+print(True and False)
+
+print(5>3 and 10<20) 
+a=5
+b=3
+print(a>b and b<a)
+age=20
+marks=80
+print(age>=18  or marks>=60)
+print(not True)
+print(not False)
+print(not age>=18)
+print(not(age>=18 and marks>=60))
+# more knowledge
+
+# True and expression 
+a=10 
+b=20 
+print(True and a>b)
+print(False and 100)
+print(100 and True)
+print(100 and True)
+print(True and 232)
+print(False or 100)
+name="shanthanu"
+print("shantanu" and True)
+print(True and 'shanthnu')
+print(True and "shanthanu")
+
+print(not 100)
+
+

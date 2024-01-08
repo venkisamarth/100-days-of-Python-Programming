3f9b69a05401925cfcb5c51041482fb65c2795ec
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-01-08 23:01:15 +0530

    Add files via upload

---
+++ print_function.py
@@ -0,0 +1,37 @@
+# input() function is used to take input from user
+# output() funcion is used to print output on the screan 
+print("hello")
+print(20.4)
+print(20,3,"hello",20)
+pi=3.14
+print(pi)
+print("value of pi is:", pi)
+
+# print():
+# The print() function prints the given object to the standtard output device (screen) or to the text strem file
+# Syntax (*objects,sep="",end="\n",file=sys.stdont,flush=False)
+
+#object= object to the printed * indicates that there may be more than one object
+# sep=object are separeted by sep by sep default value: 
+
+# end =end is used to print at the last 
+
+# file=uses write method to write in file deficult is sys.student
+
+# flush if True ,object is flushed default value is False
+
+# flush object is stored  at memory 
+
+print("hello",20)
+print("hello",20,sep="@")
+print("hello",20,sep="@$")
+print("hello",20,sep="\t")
+
+# print(*object ,sep=" " ,end="\n")
+print("welcome")
+print(" to")
+print("codeyug")
+
+print("welcome",end="  ")
+print("to",end=" ")
+print("codeyug")

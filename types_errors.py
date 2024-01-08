77e495d0fd19e1a8f28e9363b48880add092b911
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-01-08 23:39:01 +0530

    Add files via upload

---
+++ types_errors.py
@@ -0,0 +1,48 @@
+# #type of errors in python
+# # understanding error message
+# ValueError
+# NameError
+# SystemError
+# KeyError
+# TypeError
+# IndentationError
+# IndexError
+# types of erros in python
+# # syntax error
+# symatic error
+#runtime error
+
+#syntax error
+# It means our writen code is not correct according  to python
+# print(hello)
+# runtime error
+# It means at the runtime of the programs 
+# print(3/0)
+#symatic error
+# a=20
+# b=20
+# print(a+b/3)
+# type error
+# TypeError
+
+#occurs when you try to combain the two onjects of diffrent data type
+a=10
+b=20
+print(a+b)
+NameError
+a=10
+result=a+b
+print(result)
+# you have used a varible before it has value
+
+ValueError
+a="hello"
+print(a)
+
+# indentaion
+ # indentation error
+# Indentaion error
+a=[1,2,3,4]
+print(a[5])
+
+

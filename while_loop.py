dcc0987dd48fa7a2a193091fb61176f4e40305bc
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-01-09 07:34:37 +0530

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,28 @@
+# looping in the python
+print("codeyug")
+print("codeyug")
+
+#looping are used in programming to reapeat a specific block of code 
+#in a python we ahve two types of loops 
+#while 
+#for loop 
+
+1# while condition:
+    # block of code
+
+#while loop is used to iterate over a block of code as long as conditon as conditon  is True
+#In the while loop condition is checked first if conditon is True body of while is executed
+i=2
+while i<=10:
+    print(i)
+    i=i+1
+
+# printig the venkatesh 100 times
+i=0
+while i<=100:
+    print("venkatsh")
+    i=i+1
+i=1
+while i<=10:
+    print("2","x",i,"=", 2*i)
+    i=i+1

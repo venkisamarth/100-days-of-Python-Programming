1b3680e0e50efa3ee7298ac1b535699dc1d7d2ab
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-01-09 21:24:16 +0530

    Add files via upload

---
+++ range_fun.py
@@ -0,0 +1,36 @@
+# range() function
+# range() function return an immutable sequnce of members between 
+# the given start and stop int 
+
+# syntax (start ,stop,step)
+# start =starting position starting integer of range sequance
+# stop =ending position .sequnce  and end at stop 
+# step increment between each integer in sequnce
+
+print(list(range(0,10,1)))
+print(list(range(0,10,2)))
+print(list(range(5,1)))
+print(list(range(5,1)))
+print(list(range(5)))
+print(list(range(10,0,-2)))
+
+print(list(range(100,120,5)))
+# position of number is called index
+
+num=range(10,20,2)
+print(num[0])
+print(num[1])
+print(num[2])
+print(num[3])
+print(num[2])
+print(num[4])
+print(num[-1])
+print(num[-2])
+
+print(range(10,20,2))
+
+print(num[3])
+
+# write the program to print the even numbers
+print(list(range(0,20,2)))
+

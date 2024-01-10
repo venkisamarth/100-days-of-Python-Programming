53fa8866c9f7d882c9dc31ae07254966ea600f6c
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-01-11 00:57:40 +0530

    Add files via upload

---
+++ ord_function.py
@@ -0,0 +1,25 @@
+# ord() this function method return an integer representig unicode for th given character
+    
+# #     syntax 
+# # ord(character)
+# # character  A character (string of length one )
+# # takes only one character 
+# ord("A")
+# ord("a")
+
+
+# ord() function in python 
+print(ord("A"))
+print(ord("#"))
+
+# char() function 
+# This inbult method return a character from an integers(represents unicode)
+
+# systax 
+# char(integer)
+# if the value of integer is out of range value  error will be raised 
+print(chr(103))
+print(chr(97))
+print(chr(23))
+
+print(chr(31))

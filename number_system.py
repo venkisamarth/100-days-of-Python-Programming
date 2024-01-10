6006f9e5d82de0389e20c1bac254f95b7acd1d15
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-01-11 00:14:24 +0530

    Add files via upload

---
+++ number_system.py
@@ -0,0 +1,51 @@
+# # python oct(),hex(),bin() python 
+# # inbuilt function for integers conversion 
+# bin()
+# oct()
+# hex()
+# # inter -conversion among integers
+# # A number prefix ob/oB is considerd as binary 
+# # a number with prefix Oo/OO is considerd as octale
+# # A number with prefix Ox/Ox os considerd as hexadecimal 
+
+# Built in function for inter-conversion 
+# bin()
+# oct()
+# hex()
+# int()
+
+# bin()
+# syntax 
+# bin(num)
+
+# we can also pass hexadecimal  is to calculated 
+# octal number in it 
+
+# num=5 
+# print("binary of 5 is:",bin(num))
+
+# print(type(bin(num)))
+
+# print(bin(23))
+# print(bin(232))
+# oct()
+
+# converts and returns octal string equivalent to passed integers
+# syntax 
+# oct(num) number whose hexadecimal is to be calculated 
+
+# hex() 
+# converts and returns  hexadecimal string equivalent to passed integers
+# syntax 
+# # hex()
+# num=5
+# print(oct)
+# g=(oct(num))
+# print(type(g))
+
+# hexadecimal
+f=hex(15)
+print(f)
+print(type(f))
+
+

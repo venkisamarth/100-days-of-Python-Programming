4fe9dc7d107c3317e9f9d893eafac9b32632d71d
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-01-10 23:22:07 +0530

    Add files via upload

---
+++ Ternary_operator.py
@@ -0,0 +1,69 @@
+# what is ternary operater in programing 
+# what is ternary operater in python 
+# types of operater 
+# unary 
+# binary 
+# ternary
+# unary opearater
+# op=10
+# print(op)
+
+# Binary operater
+# op1=11
+# op2=20
+# print(op1+op2)
+# what is ternary operater 
+# syntax 
+# conditaion value _if_if_else
+# oparand secondary 
+# conditional operater
+
+
+# condiation operater
+
+# age=20
+# print("you can vote") if (age>=18) else "you can"
+# print("message")
+
+# # walrus operater in python
+# a=100
+# data=[10,20,30,40,50,60]
+# n=len(data)
+# i=0
+# while i<n:
+#     print(data[i])
+#     i=i+1
+# i=0
+# while i<(n:=len(data)):
+#     print(data[i])
+#     i=i+1
+
+# i=-1
+# while (i:= i+1)<(n:=len(data)):
+#     print(data[i])
+
+
+
+# data=[10,20,30,40,50,60,70,80,90]
+# n=len(data)
+# while n>0:
+#     print(data.pop())
+#     n-=1
+# print(data)
+
+# data=[10,20,30,40,50,60]
+# while(n:=len(data))>0:
+#     print(data.pop())
+#     n-=1
+#     print(data)
+
+# while True:
+#     ans=input("Do you want to continue(y/n):".lower())
+#     if ans!="y":
+#         break
+#     print("process the request")
+
+while(ans:=input("Do you want to continue(y/n:)".lower()=="y")):
+    print("process the request")
+
+    

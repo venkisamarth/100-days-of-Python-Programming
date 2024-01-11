594cd1408f138575668650081d3ac02dd38b1775
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-01-11 22:28:09 +0530

    Add files via upload

---
+++ replace_.py
@@ -0,0 +1,12 @@
+# replace()
+
+#  the replace method returns a copy of string where all occurence of sub_string is replace with another substring
+#  string replace(old_value,new_value,count)
+# replace() method
+# message=("c is the object oriented programing language")
+# message=message.replace("c","python")
+# print(message)
+
+greet=("hello venki")
+greet=greet.replace("hello","how are you ")
+print(greet)

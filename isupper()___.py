5fe9ed23bf2162459060e6994b3a78bd06c7330c
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-01-13 12:32:59 +0530

    Add files via upload

---
+++ isupper()___.py
@@ -0,0 +1,51 @@
+# True if all alphabets are in uppercase
+# False if any alphabets  is in lowercase
+
+# systax 
+#    string.upper()
+
+# str1="welcome to myhome"
+# print(str1.isupper())
+
+# print(str1.islower())
+
+
+# str1="WELCOME TO THE MYHOME"
+# print(str1.islower())
+# print(str1.isupper())
+
+# islower()
+
+# True - If all alphabets are in lowercase 
+# Flase -if any alphabets is in upper
+
+# str1="codewithharry"
+# str2=str1.islower()
+# print(str1)
+
+# str1="code@islower"
+# print(str1.islower())
+
+# istitle()
+
+# True if String is titled string 
+# False If string is not fitled string 
+
+# System
+str1="python is easy"
+print(str1.istitle())
+str1=(str1.title())
+print(str1.istitle())
+print(str1)
+
+
+
+
+
+
+
+
+
+
+
+

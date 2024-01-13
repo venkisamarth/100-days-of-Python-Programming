1dc800e6d15d775737935d3b390274d3fea885ff
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-01-13 12:48:20 +0530

    Add files via upload

---
+++ isspace()__.py
@@ -0,0 +1,41 @@
+# isspace()
+# isidentifier()
+# isprintable()
+
+# True if string  is valid identfier  else returns false
+# systax 
+# # string.isidentifier()
+
+# # This does't take any arguments 
+
+# string ="abddfe"
+# print(string.isidentifier())
+# string ="1223__ asdg"
+# print(string.isidentifier())
+# # ispritable()
+
+# True if all character are printable or the string is emplty 
+
+# # This method doe't  take any argument that occupies 
+# # printable space on the screen 
+ 
+# str1="python"
+# True- If all character are spaces 
+# False if all the cahracter are spaces 
+# systax 
+
+#  string.isspace()
+
+
+# This method does't take any parameter 
+
+str1="   "
+print(str1.isspace())
+str1="y"
+print(str1.isspace())
+
+str1="____"
+print(str1.isspace())
+print(str1.isspace())
+
+print(str1.isspace())

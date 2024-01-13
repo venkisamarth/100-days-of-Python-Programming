49cffebf22e0bc88d0f012376036f645518447b5
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-01-13 12:13:46 +0530

    Add files via upload

---
+++ isalpa()__.py
@@ -0,0 +1,26 @@
+
+# True all the character in string are alphabets else return  False
+# #  systax 
+# msg="welcome"
+# print("welcome")
+# msg="welcome"
+# print(msg.isalpha())
+
+
+"""isdigit()
+
+True all character in string are digits ,else return Flase 
+
+systax 
+ string .isdigit()
+"""
+# msg ="12345"
+# print(msg.isdigit())
+# print(id(msg))
+
+mob_num=input("enter mobile number")
+if mob_num.isdigit()==True:
+    print("mobile  number is valid ")
+else:
+    print("invlid number")
+

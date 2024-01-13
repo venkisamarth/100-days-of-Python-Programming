ed9506fd6c5b8cc95380ad7b002b1ee54673efaf
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-01-13 11:19:11 +0530

    Add files via upload

---
+++ upper()method in python.py
@@ -0,0 +1,51 @@
+# upper()method in python
+# the upper method converts all the lower letter to the upper letter  the given string 
+# and returns the new copy of the string 
+
+# systax'
+
+#  string.upper()
+
+
+# str1="Hello,welcome"
+# str2=str1.upper()
+# print(str1)
+# print(id(str1))
+# print(id(str2))
+
+# str1="Hello world!"
+# print(str1)
+# user=input("enter username")
+# correct_user="shantanu"
+# if user==correct_user.upper():
+#     print("valid user name")
+
+# lower()
+# the string lower() method converts all uppercase character in string into lowercase character and 
+# returns  a new copy 
+
+# syntax 
+# This method does't  take any parameter
+
+# s1="codeyug"
+# s2=s1.lower()
+# print(s1)
+
+# print(id(s1))
+# print(id(s2))
+
+# swapcase()
+
+# The swapcase  converts the all the uppercase to lowercase and the lowercase to the uppercase
+# this dose't to take any parameter
+
+# systax 
+#    string.swapcase()
+
+# This is don't take any argument 
+
+s="codeYOG"
+s1=s.swapcase()
+print(s1)
+
+print(s1)

b6d8de9f20e037a696e14a7f11af5216350b84e2
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-01-13 13:20:08 +0530

    Add files via upload

---
+++ mem driver _program.py
@@ -0,0 +1,15 @@
+num1=int(input("enter first number"))
+num2=int(input("enter second number "))
+print("1.additon\n2.subtraction\n3.multiplication\n4.divison")
+choice=int(input("choice from above"))
+if choice ==1:
+    print("addition is:",num1+num2)
+elif choice==1:
+    print("the subtraction is :",num1-num2)
+
+elif choice==3:
+    print("subtraction is:",num1-num2)
+
+elif choice==4:
+    print(print('division is : ',num1/num2))
+    

c94889ded9b0086587b078a9b2d6c1946dbc2319
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-01-13 12:02:32 +0530

    Add files via upload

---
+++ isalnum__.py
@@ -0,0 +1,16 @@
+# isalnum()
+# all character in string in string are alpha-numeric else return False 
+
+msg="welcome"
+print(msg.isalnum())
+
+
+msg="welcome123"
+print(msg.isalnum())
+
+msg="welcome_123"
+print(msg.isalnum())
+
+msg="welcome232"
+print(msg.isalnum())
+

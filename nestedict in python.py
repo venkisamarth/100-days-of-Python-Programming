98de48ac12fee67f7b443e55f2aa72f380f95840
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-01-20 12:00:45 +0530

    Add files via upload

---
+++ nestedict in python.py
@@ -0,0 +1,19 @@
+# conditions 
+# 1  we cannot use dictinary  as key 
+# 2 can use dictionary as value 
+# my_dict ={{'one':1}:"fist"}
+# print(my_dict)
+
+# a a dictionary within another dictionary is called as nested dictionary 
+#  nested_dict={"dictA":{"key1":value1,'key2':value2}
+#               "dictB":{"key1":value1,"key2":value2}}
+
+student ={"amar":{"age":22,"gender":"male","fees":1300},
+          "rakes":{"age":22,"gender":"male","fees":1300},"jay":12000}
+
+print(student)
+
+
+
+
+

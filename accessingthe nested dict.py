f9ce9d9cfbc1516cedaad28760ae35bbb95f6538
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-01-20 12:09:24 +0530

    Add files via upload

---
+++ accessingthe nested dict.py
@@ -0,0 +1,10 @@
+student ={"jay":{"age":22,"marks":97},"raj":{"age":23,"marks":98}}
+print(student["jay"])
+
+print(student["jay"]['age'])
+
+print({"jay":{"age":23,'marks':89},"raj":{"age":13,"marks":89}})
+print(student)
+
+print(student['jay']["marks"])
+

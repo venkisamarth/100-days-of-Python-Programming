6f0c338be76d92f2bdf70fee30dbaff8b28112b2
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-01-24 22:31:45 +0530

    Add files via upload

---
+++ working_with_bytes.py
@@ -0,0 +1,24 @@
+print(ord('A'))
+print(chr(65))
+data=[10,20,30,40,50]
+print(bytes(data))
+print(chr(10))
+print(chr(12))
+print(chr(14))
+print(chr(14))
+print(chr(16))
+
+data=[65,97,98]
+print(bytearray(b'Aab'))
+
+
+#diffrent opertaions
+
+print(bytes("shatanu"))
+print(bytes('shantanu'))
+str1="shatanu"
+b=bytes("shatanu",encoding="utf-8")
+print(b)
+
+
+

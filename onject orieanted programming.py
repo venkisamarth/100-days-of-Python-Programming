58e1f1af00f39d9fdec792c5bbcb5e97ec56b54d
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-02-03 20:03:22 +0530

    Add files via upload

---
+++ onject orieanted programming.py
@@ -0,0 +1,13 @@
+# syllabus and pre-requarments of oops 
+
+# Basics of oops 
+# Tyeps of variables and methods 
+# inheritance 
+# polymorphism 
+# Encapsulation 
+# Abstraction 
+# interface 
+# mini-project on oops concept 
+# interviwe question 
+ 
+

a0f59c1d6ce7c8204beaf1d883b44dcb7ea5be4f
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-02-05 23:21:25 +0530

    Add files via upload

---
+++ make method provate.py
@@ -0,0 +1,10 @@
+class Finace:
+    def __init__(self): 
+        self.__revenue=100000
+        self.number_of_sales=144
+    def __display(self): 
+        print(self.__revenue)
+
+f1=Finace()
+f1.__display()
+

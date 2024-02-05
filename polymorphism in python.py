b14c6ec2f87e1ccb99dbba375c189bbda1fbd2bd
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-02-05 23:32:07 +0530

    Add files via upload

---
+++ polymorphism in python.py
@@ -0,0 +1,24 @@
+#example of polymorphisam
+#polymorphisam in built in function 
+
+#It is an ability  of python object to take many forms 
+#If a varaible,object,method ,perform diffrent  behaviours to situation is called as polymorphisam 
+ 
+# + :- python object 
+print(10 +20 )
+print("heloo"+"world")
+#len() function 
+#revenue function 
+print(len("hello") )
+# print(len("h","w","e"))
+print(len("Dicationary "))
+
+# reversed 
+emp=["jay","viru","ram"]
+company ="infosys"
+for i in reversed(company): 
+    print(i)
+for i in reversed(emp):
+    print(i)
+
+

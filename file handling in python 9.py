b3ec5add8780474ecb41151d5b20b789a2890291
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-02-09 23:56:07 +0530

    Add files via upload

---
+++ file handling in python 9.py
@@ -0,0 +1,30 @@
+# ways of closing files 
+# normal way 
+# using exception  handling 
+# with statemet 
+
+# 1 normal way 
+
+# f=open("file name",mode="r")
+# f.close()
+# # using exception hadling 
+# try: 
+#      f=open("filename",mode="r")
+#      # oeprations 
+# finally: 
+#      f.close()
+
+# with stamet : 
+# with open("filename",mode="r")as f : 
+#     # operatios 
+
+#     with open("filename",mode="r") as f: 
+#         data=f.read()
+#         print(data)
+
+
+
+
+   
+
+

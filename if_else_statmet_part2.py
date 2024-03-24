6a128d2b77d8f18b841b21a3a2df5ca43d751cd2
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-03-24 12:25:07 +0530

    Add files via upload

---
+++ if_else_statmet_part2.py
@@ -0,0 +1,193 @@
+# num=10 
+# if num>=0: 
+#     print("positive")
+
+# else: 
+#     print("Negative")
+
+
+# # check if the number is even or odd 
+# num=5 
+# if num%7==0: 
+#     print("Even")
+# else: 
+#     print("odd")
+
+# #Determain the greater of the two numbers 
+# num1=10 
+# num2=12
+# if num1>num2: 
+#     print("num1 is greater")
+# elif num1==num2: 
+#     print("Both the numbers or equal")
+# else: 
+#     print("num2 is greater")
+#check the number is divisible by both 3 and 5
+
+# num=15 
+# if num %5==0 and num%3==0: 
+#     print("The number is divisible by both 3 and 5")
+# # else: 
+# #     print("Not divisible by both 3 and 4")
+
+# # checking if the number is within the given range:
+# num=12
+# if 20<=num<=30: 
+#     print("withinn range")
+# else: 
+#     print("out or range")
+
+# # cheking if a year is a leap year: 
+# year=int(input("Enter the year: "))
+# if (year%4==0 and year%100!=0) or (year%400==0): 
+#     print("Leap year")
+# else: 
+#     print("not a leap year")
+
+# determain the traingle based on the side lenght
+
+# char ="a"
+# if char in "aeiou": 
+#     print("Vowel")
+# else: 
+#     print("consonant")
+
+#cheking if the person is eligible to vote: 
+# age=19 
+# if age>=18: 
+#     print("Eligible to vote")
+# else: 
+#     print("Not eligible to vote")
+
+#check if the numbers is positive or negative in python 
+# num=13 
+# if num>0: 
+#     print("positive")
+# elif num==0: 
+#     print("Zero")
+# else: 
+#     print("Negative")
+
+# cheking if a year is a century year or not 
+# year =2000
+# if year %100==0: 
+#     print("century year")
+# else: 
+#     print("NOt a century year")
+
+
+# num=25 
+# if int(num**0.5)**2==num: 
+#     print("perfect square")
+# else: 
+#     print("NOt a perfect square")
+
+# checking if a number is prime number ir composite: 
+
+# num=13
+# if num>1: 
+#     for i in range(2,int(num/2)+1): 
+#         if(num%i)==0: 
+#             print("coposite")
+#             break
+#     else: 
+#         print("prime")
+# else: 
+#     print("Neither prime nor composite")
+
+# cheking larges among the three numbers :
+# a,b,c=10,20,30
+# if a>=b and a>=c: 
+#     print("Largest:",a)
+# elif b>=a and b>=c: 
+#     print("Largest:",b)
+# else: 
+#     print('Largest:',c)
+
+# cheking if the year is leap year and divisible by 400 
+# year =2000
+# if year% 400 == 0: 
+#     print("Leap year and divisible by 400")
+# else: 
+#     print("Not a leap year or not divisibole by")
+
+# scheck if the number is fibonaccin number 
+# num=21 
+# a,b=0,1 
+# while b<num: 
+#     a,b=b,a+b 
+# if b==num: 
+#     print("Fibonacci")
+# else: 
+#     print("Not Fibonacci")
+
+
+# check if a number is within a specific range
+
+# num=35 
+# lower_limit,upper_limit=20,50
+# if lower_limit<=num <=upper_limit: 
+#     print("within the range")
+
+# else: 
+#     print("outside the range")
+
+# # cheking if a string is palindrome:
+# string ="rader"
+# if string ==string[::-1]: 
+#     print("palindrome")
+# else:
+#     print("Not a palindrome")
+
+# #cheking if year is a leap year and divisible by 4 but not by 100 
+# year =2008 
+# if (year%4 ==0 and year %100!=0) or (year%400==0):
+#     print("Leap year and divisible by 4 but not by 100")
+# else: 
+#     print("Not a leap year or not divisible by 4 but not by divisible")
+# # cheking if a number is within a certin range using nested if 
+# num=45 
+# if num>=0: 
+#     if num<=50: 
+#         print("Within the range 0-50")
+#     else:
+#         print("outside the range 0-50")
+# else: 
+#     print("Negative numbers")
+
+#cheking if a student score falls within a certine grade range: 
+# score=85 
+# if score >=90: 
+#     grade="A"
+# elif score>=80: 
+#     grade="B"
+# elif score>=70: 
+#     grade="C"
+# elif score>=60: 
+#     grade="F"
+# print("Grade:",grade)
+# check if the string is empty or not 
+# string ="Hello"
+# if string: 
+#     print("String is not empty")
+# else: 
+#     print("String is empty")
+# #cheking if a number is between two values
+# num=25 
+# if 20<=num <=30: 
+#     print("Within the range")
+# else: 
+#     print("Outside thr range")
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ if_statment_in_python_part2.py
@@ -0,0 +1,146 @@
+# num=int(input("Enter the number:  "))
+# if num>0: 
+#     print("The number is positive number ")
+# elif num<0: 
+#     print("The number is Negative")
+# else: 
+#     print("Zero")
+
+
+# # Determine if a number is even or odd 
+
+# num=int(input("Enter the number "))
+# if  num%2: 
+#     print("Even")
+# else: 
+#     print("odd")
+
+# validation of the given year is leep year or not 
+# year=int(input("Enter the year: "))
+# if (year %4 ==0 and year %100!=0) or (year%400==400): 
+#     print(f"this the {year} leep year")
+# else: 
+#     print("Not a leap year")
+
+# varify if a number is divisible if a another number or not 
+# num1=12 
+# num2=4 
+# if num1 %num2 ==0: 
+#     print(f"{num1} is divisible by {num2} ")
+# else: 
+#     print(f"{num1} is not divisible by {num1}")
+
+# compare two numbers and detwermain which is one is greter 
+# num1=float(input("Enter the first number: "))
+# num2=float(input("Enter the second number:  "))
+# if num1>num2: 
+#     print(f"{num1} is greter then {num2}")
+# if num1 <num2:
+#     print(f"{num2} is greter then {num1}")
+# else: 
+#     print("Bothe the number is equal") 
+# check the given number is within the a cirtin range ir not 
+# num=25 
+# lower_bound=10 
+# upper_bound=30 
+# if lower_bound<=num <=upper_bound: 
+#     print(f"{num} is within the range ")
+# else: 
+#     print(f"{num} is not within the range")
+
+# #Determain if the traiangle is equalateral,isosceles based on the sides
+# side1=5 
+# side2=5 
+# side3 =5 
+# if side1==side2 ==side3: 
+#     print("Equalteral triangle")
+# elif side1 == side2 or side1 == side3 or side2 == side3:
+#     print("This is the isoscales tirangle") 
+# else: 
+#     print("scalen triangel")
+
+# Check if a number is postive ir negative 
+# num=10 
+# if num>=0: 
+#     print("This number is possitive ")
+# else : 
+#     print("this is the negative number")
+
+#check if a numbers is even or odd : 
+# num=7 
+# if num%2 ==0: 
+#     print('This is the even number')
+# else: 
+#     print("This is the odd number")
+
+#check is the person is eligible to vote or not 
+
+# age=18 
+# if age>=18: 
+#     print("you are eligible to vote.")
+# else: 
+#     print("you are not eligible to vote yet.")
+
+#check of a number is divisible by anothe number: 
+# num1=12 
+# num2=3
+# if num1%num2==0: 
+#     print("num1 is divisible by num2.")
+# else: 
+#     print("num1 is not divisible by num2.")
+# if a person can apply foe a senior docount: 
+
+# age=65 
+# if age>=65: 
+#     print("YOu are eligible for a senior discount")
+# else: 
+#     print("you are not eligible for a senior dicscount")
+
+#check if a character is a vowel ro consonat: 
+# char ="a"
+# if char in ["a","e","i","o","u"]: 
+#     print("The character is a vowel")
+# else: 
+#     print("This character is a consonant")
+
+
+#check  if the a student passed ir faild based on marks: 
+# marks=75 
+# if marks>=50: 
+#     print("congratulation! you passed the exam.")
+# else: 
+#     print("sorry, you failed the exam")
+
+#check if a number is with specified rage: 
+# num =25 
+# if num in range(10,30): 
+#     print("The number us writin the specified range")
+# else: 
+#     print(" The number is outside the specified range")
+
+#check if a year is leap year: 
+# year =2024
+# if (year % 4==0 and year%100!=0 ) or (year%400==0): 
+#     print("This is the leap year")
+# else: 
+#     print("This is not the leap year")
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+

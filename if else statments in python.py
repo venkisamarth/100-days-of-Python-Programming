c0a1d7dcbaaa9e186f2ac5ed20d62b57f5ca26d0
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-03-24 12:17:42 +0530

    Add files via upload

---
+++ if else statments in python.py
@@ -0,0 +1,23 @@
+# checking if number is posstive or negative 
+# num=int(input("input the number"))
+# if num>=0:
+#     print("The number is positive or zero")
+# else:
+#     print("The number is negative")
+# verifying if a person is eligible to vote
+# age=18
+# if age>=18:
+#     print("you are eligible to vote")
+# else:
+#     print("Tou are eligible for the vote")
+
+# determaion the largeest of two numbers 
+
+a=10
+b=5
+if a>b:
+    print("a is greater than b.")
+else:
+    print("b is greater thatn or equal to a.")
+
+

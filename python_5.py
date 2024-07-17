58c3f44c01d019cf2f359799b90e8d02a6fa1130
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-07-17 15:45:51 +0530

    Add files via upload

---
+++ python_5.py
@@ -0,0 +1,22 @@
+# python istallation 
+# # installing python on windows 
+# first check if the python is installed or not 
+
+# cmd-> python ->is not recognized as an internal or wxternal command operable prgram or batch batch file 
+# if python is already installed then it could should the version of the python 
+# after that  enter python then it shouws nothing
+
+# www.python.org 
+
+# crome 
+# python  
+# welcome to python .org 
+# downloade python -> 
+# system type 
+
+# start system type 
+
+
+
+
+
---
+++ python_7.py
@@ -0,0 +1,49 @@
+# what is IDLE? 
+#python statment and hello world program
+# continuation character(\) 
+
+# what is idle? 
+# Integrated Development Learing Env 
+# It is an IDE provided by python 
+# when you install python, IDLE application 
+# gets installed 
+# Two ways to run programs 
+# usin interactive shell 
+# using a scipt file 
+
+# python hello world 
+#print ("hello world ") 
+# in python  
+# print("Hello World")
+
+# # in c programming 
+# # include<stdio.h> 
+# int main(void){
+#     printf("Hello world")
+# }
+
+# #in c++ programming 
+# # include<iostream.h>
+# int main()
+# {
+#     std::cout<<"Hello world";
+#     return 0;
+
+# }
+
+# # in java  programming 
+# class HellowWorld{
+#     public static void main(String[] args){
+#         system.out.println("Hello world")
+#     }
+# }
+# }
+
+print("Hello world")
+
+
+
+
+
+
+

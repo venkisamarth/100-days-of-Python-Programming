097904c6064143516b430079be2b8e189b73302b
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-10-09 23:26:37 +0530

    Add files via upload

---
+++ python_1.py
@@ -0,0 +1,37 @@
+#what is computer programming 
+#---set of instruction for the computer to follow and achiver certine goal or task
+#set of instruction for the computer to follow and achive certine goal or task 
+#Below terms we have 
+#Program set of instruction for computer to follow 
+# programming :- process of ddveloping program 
+# programer:- person who develop programs 
+# programing  language :- computer language for instruction 
+
+
+# # process of programming :-
+# 1 problem defination
+# 2 algorithem design and planning 
+# 3 coding 
+# 4 Testing and Debugging 
+# 6 Maintenance
+
+
+#programming languages:-
+#each programming language has its own syntax and rules 
+
+# low level language :- 
+# These language are closer to  the machine's hardware and provide more direct  control over the computer's resources 
+# assembly level language 
+
+#high level language 
+
+
+
+
+
+
+
+
+
+
+
---
+++ python_2.py
@@ -0,0 +1,63 @@
+#what is python 
+#python is a high level  prgramming language like c and c++
+# python is a simple , high level, interprited ,general, purpose,dynamically typed and object oriented 
+# programming  language created by Guido Van Rossum in 1991
+
+
+# python is simple :-
+# -python us easy to use 
+# -python program are simple to write and hence it is easy to learn 
+# -lots of built-in modules . pakages and Frameworks
+
+# in java we have to create a class to print the hello world  on disply 
+# like hello world in java 
+
+# class HeloWorld{
+#     static public  void main(string args[]){
+#         System.out.printlin("Hello world ");
+#     }
+
+
+# in c++ // hello world program  
+
+# #include<iostream.h>
+# main(){
+#     cout <<"Hello world! <<endl";
+#     return 0;
+# }
+# but in python
+# Hello world in python 
+# print("hello world")
+
+# python combaines remarkable power with very clean simple and compact syntax 
+
+
+# python is a high level lanuage 
+#More user-friendly 
+# There is automatic memory  managment 
+#Rich set of libraries and function ex. len(x)
+# 
+
+
+
+# python is interproted language:- 
+#Final execution of python program is down by interpreter 
+#interpreter scan the python  program line by line
+
+
+#python is the general python program language 
+#pytho is used for web development machine learning artificial intelligence data analysis data science , web scrapping scripting scientific computing softewar dev etc 
+# python is dynamicaly typed language 
+# Like c or c++ we don't need to specify datatype of identifiers 
+# python eveluated datatype at runtime
+
+
+# python is object oriented program language 
+# python supported nearly all concepts of object oriented style 
+
+
+# python is a simple, high level interproted general purpose  dynamical typed and object oriented  program language created by Guido Van Rossum in 1991. 
+
+
+
+

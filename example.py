3be47f2254c34681ae919d9cab9111277fe2baa9
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-11-25 23:57:05 +0530

    Add files via upload

--- example.py
+++ example.py
@@ -1,1 +1,19 @@
-print("hello world")
+# import example1 
+# print(example1.x)
+# example1.personl_info()
+
+# # example1.personl_info()
+# from example1 import * 
+# print(dir())
+# x=1000 
+# display("this is the normal the it coul")
+
+__all__=["x"]
+
+__all__=["x","personal info personl_info "]
+
+
+
+
+
+
--- func1.py
+++ func1.py
@@ -1,73 +1,35 @@
-#prodedural function in python
-#problems with procedural  programming approch
+# 1 Functional Programming Tutorial 
+# functional and its need functiona? 
+# parameters and arguments 
 
-# #1 large size of program and less efficiency 
-# difficalty in code managemnt 
-# you can't migrate  code to other project 
-# Problems  when requirement incresed 
-
-
-
+# return statment in python 
 a=10
 b=20
 result =a+b
-print("Addtion is: ",result)
-
-a=1000
-b=2000
-result=("Addition is:",a+b)
+print(result )
+print(result )
+a=100
+b=200
+result =a+b
 print(result)
+# this is called as the procedural uproch 
+# problems with procedural programming approch..
+# 1 large size  of program and less efficiencey
+# 2 difficulty in code management 
+# 3 you  con't migrate code to other pprojet 
+# problems when requarments is requared 
 
-#functional programming
-# def add(a,b):
-#     return a+b
-# print(add(3,4))
+def addition(a,b):
+    print("welcome!")
+    print("Addtion is",a+b)
+for i in range(100):
+        a=int(input("Enter first Number: "))
+        b=int(input("Enter the value of b: "))
+        addition(a+b)
 
-def addition(a,b):# function defination in python
-    print("Welcome")
-    print("Addtionis:",a+b)
-
-print(addition(2,5))# function caling
-print(addition(3,5))
-print(addition(100,200))
-
-# def add(a,b):
-#     print("Welcome")
-#     print("Addition is",a+b)
-#     for i in range(100):
-#         a=int(input("Enter  first Number: "))
-#         b=int(input("Enter second Number: "))
-#         print(add(a,b))
-# print(add(2000,24343))
-
-
-# what is function in python
-#function is a organised block of reusable code  
-# function can be called any number of times 
-# Function contaion a set of instruction whcih perforem specific test  example addtion in above example 
-# Bank application 
-# withdraw credit take_loan link_with
-# other resons of using function: 
-
-# Testing and Debugging :- 
-# function make it easier to test and debug spcefic  parts of your code
-
-# you can isolate and focus on  individual funtion without  dealing with the entire program's complexity 
-# In testing we can debug programs's  complexity 
-
-# In testinf we can debug individual components
-
-#  Improved readabality 
-# Effecient code managment 
-# functional programming provieds concepts like recursion higher order functions decorators whcih are helfucl for solving complex problems programming
-   
-
-# there are tow types of function 
-# built in fucntion  and user define function 
-
-str1="venkatesh"
-print(len(str1))
-
+# print(addition(10,20))
+# print(addition(30,34))
+# print(addition(100,200))
 
 
 

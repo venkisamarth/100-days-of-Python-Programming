2e0b6e1a12fa6771803717d5150da5abf167a37a
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-12-26 23:59:47 +0530

    Add files via upload

---
+++ python_12.py
@@ -0,0 +1,45 @@
+#Data types in python 
+#Two check data type of the  function 
+#type() funciton 
+#isinstance() function 
+
+
+#data type is the just the catogory of data  
+#it defines  possible operation:- 10,'shantanu'
+ # below are datatypes available in python 
+
+#-numeric types:- int, float , complex (special type)
+# sequnce types :- str,list ,tuple 
+# there is no array opetion in python list will work as array in the arrya  that could be the 
+#using exsternal modul we can create an arrya in the python this could be the normal the it could be the progrmming 
+#array module numpy array 
+# maping type colliectio of key VALUE PARIS 
+# BOOLEAN TYPE  bool 
+
+# Binarly type:- Nonetype 
+# range type in 
+#binary type  in python  :-bytes, bytearray
+
+# settype in python :-set frozenset
+age=23
+print(type(23))
+print(type(age))
+#rvry data type has its own class this is the normal then it could be the normal the it sould be
+name="h"
+print(type(name))
+
+# there is nor char  data type in  python there is only str data type in python 
+# but in c  and C++ in this is the normal the it could be the normal in python 
+age=[23,23,34,43,12,23]
+print(type(age))
+my_tupe=(1,2,3,4,4,"venki",4)
+print(type(my_tupe))
+
+
+#isinstance is used to conferm whter it is whcih data type in python 
+print(isinstance(1,int))
+print(age,float)
+
+result =isinstance(age,int)
+print(result)
+
---
+++ python_13.py
@@ -0,0 +1,48 @@
+#what will we learn 
+
+#-- introduction  to all data types their syntaxes and usage 
+# cheking datatype using type() function 
+# # indexing in list and tuple 
+
+# numeric types 
+# int 
+# float
+# complex 
+
+# int 
+# numeric types 
+# contains intergers 
+# can  be of any lenght
+
+# ex. ... -3 ,-2,-1,1,2,3...
+# ex. target =180
+print(type("Hello world"))
+print(type('hello world'))
+print(type('''hello world'''))
+print(type(""""Hello world"""))
+
+print("""
+      like 
+      share 
+      subscribe """)
+name ="shantanu"
+print(len(name))
+age ='12'
+print(type(age))
+print(len(age))
+print(name[4])
+
+# sequnce 
+# list 
+# tuple 
+# str
+
+# list is the orderd collection of object then it could be the normal then it sould be the programming 
+
+
+# list of data's haing differenct data tyeps 
+# orderd mutable sequnce of intems 
+#
+
+
+
